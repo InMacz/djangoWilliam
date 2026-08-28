c9eff4adea87f773d3c8fabb67ebcae8a577cbe9
William <inmacz321@gmail.com>
2026-08-27 22:43:59 -0300

    fix btn voltar

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import index, consContato
 urlpatterns = [
     path('', index, name = 'index'),
+    path('consContato/<int:pk>', consContato, name="consContato"),
 ]
